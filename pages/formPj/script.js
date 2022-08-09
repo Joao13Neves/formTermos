bd8d8e655e566ee8c8f7bcca13c2f8e5cb2638ab
pedro.fischer <pedro.fischer@modalgr.com.br>
2022-08-09 13:15:43 -0300

    teste dos valores

--- pages/formPj/script.js
+++ pages/formPj/script.js
@@ -58,7 +58,9 @@ const Form = {
         const elementsFormDocument = document.querySelectorAll('#form-document input');
         const elementsFormAdress = document.querySelectorAll('#form-address input');
         const elementsFormItems = document.querySelectorAll('#form-items input');
+        
 
+        
         try {
 
             elementsFormDocument.forEach(element => {
@@ -73,7 +75,14 @@ const Form = {
                 Form.validateEmpty(element.value, element.placeholder)
             });
             
-           
+            const documentArray = [];
+            const addressArray = [];
+            const itemsArray = [];
+
+            elementsFormDocument.forEach(item => documentArray.push(item.value))
+            elementsFormAdress.forEach(item => addressArray.push(item.value))
+            elementsFormItems.forEach(item => itemsArray.push(item.value))
+            
             
         } catch(error) {
             swal("Atenção!", error.message)
