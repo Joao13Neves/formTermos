6368abd2838e8d6726e4f3e3d3a03e15bad35d2d
Fischer <pedro.fischer22@gmail.com>
2022-08-18 00:57:14 -0300

    deixando a parada cabulosa

--- pages/form/script.js
+++ pages/form/script.js
@@ -1,5 +1,163 @@
+const ENUM_REQUERIMENT_TYPE = {
+    AQ: 'AQ',
+    CHG: 'CHG',
+    DEV: 'DEV'
+}
+
+const ENUM_CONTRACT_TYPE = {
+    CLT: 'CLT',
+    PJ: 'PJ',
+    ESTAG: 'ESTAG'
+}
+
+const ENUM_STYLES = {
+    DISPLAY_NONE: 'none',
+    DISPLAY_BLOCK: 'block',
+    PURE_FORM_STACKED: 'pure-form pure-form-stacked',
+    FORM_DOCUMENT: 'form-document',
+    FORM_TITLE: 'form-title',    
+    PURE_G: 'pure-g',
+    PURE_U_1_MD_1_3: 'pure-u-1 pure-u-md-1-3',
+    CONTRACT_NAME: 'contract-name',
+    PURE_INPUT_1: 'pure-input-1',
+    FORM_ADDRESS: 'form-address',
+    CONTRACT_TYPE: 'contract-type',
+    PURE_FORM: 'pure-form',
+    FORM_ITEMS: 'form-items',
+    ITEMS_WRAPPER: 'items-wrapper',
+    ADD_ROW: 'add-row',
+    PURE_U_1_MD_1_3_MG_T_B_10: 'pure-u-1 pure-u-md-1-3 mg-t-b-10'
+}
+
+const SELECTORS = {
+    IS_CNPJ: '#isCnpj',
+    ITEMS: 'items',
+    FORM_DOCUMENT_INPUT: '#form-document input',
+    FORM_ADDRESS_INPUT: '#form-address input',
+    FORM_ITEMS_INPUT: '#form-items input',
+    CONTRACT_TYPE_CLT: '.contract-type-clt',
+    CONTRACT_TYPE_PJ: '.contract-type-pj',
+    CONTRACT_TYPE_ESTAG: '.contract-type-estag',
+    CONTRACT_NAME: '.contract-name',
+}
+
+const ENUM_ATTRIBUTES = {
+    CLASS: 'class',
+    ID: 'id',
+    TYPE: 'type',
+    VALUE: 'value',
+    TYPE_TEXT: 'text',
+    TYPE_NUMBER: 'number',
+    TYPE_EMAIL: 'email',
+    FOR: 'for',
+    PLACEHOLDER: 'placeholder'
+}
+
+const ENUM_ATTRIBUTES_TEXT  = { 
+    MULTI_FIRST_NAME:'multi-first-name'
+}
+
+const ENUM_PLACEHOLDER = {
+    NACIONALIDADE: 'Nacionalidade', 
+    DOCUMENTO_CNPJ: 'Documento (CNPJ)',
+    DOCUMENTO_CPF: 'Documento (CPF)',
+    FUNCAO: 'Função',
+    NOME: 'Nome (NOME)',
+    NOME_EMPRESA: 'Nome (EMPRESA)',
+
+    CEP: 'CEP', 
+    ENDERECO: 'Endereço',
+    NUMERO: 'Número',
+    COMPLEMENTO: 'Complemento', 
+    BAIRRO: 'Bairro',
+    CIDADE: 'Cidade',
+    UF: 'Uf',
+    
+}
+
+const TAGS = {
+    FORM: 'form',
+    DIV: 'div',
+    FIELDSET: 'fieldset', 
+    LEGEND: 'legend',
+    LABEL: 'label',
+    INPUT: 'input'
+}
+
+const DESCRIPTIONS = {
+    PERSONAL_DATA: 'Dados Pessoais',
+    COMPANY_DATA : 'Dados da Empresa',
+    PATRIMONY: 'Patrimônio',
+    ITEMS: '-INFORME: Nome/número do equipamento',
+    NO_FIELD_REMOVE: 'Não existe field para remover',
+    WARNING: 'Atenção!',
+    CLT: 'CLT',
+    ESTAG: 'Estagiário',
+    PJ: 'Pessoa Jurídica',
+    NOME: 'Nome',
+    NOME_EMPRESA: 'Nome Empresa',
+    FIELD: ' O campo ',
+    NOT_NULL: ' Não pode ser nulo! '
+}
+
+function buildCollaboratorForm(contractType) {
+
+    const form = document.createElement(TAGS.FORM);
+    form.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_STYLES.PURE_FORM_STACKED); 
+    form.setAttribute(ENUM_ATTRIBUTES.ID, ENUM_STYLES.FORM_DOCUMENT);
+
+    const fieldSet = document.createElement(TAGS.FIELDSET); 
+
+    const legend = document.createElement(TAGS.LEGEND);
+    legend.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_STYLES.FORM_TITLE);
+    
+    if(contractType == ENUM_CONTRACT_TYPE.CLT) {
+        legend.textContent = DESCRIPTIONS.PERSONAL_DATA; 
+    }
+
+    if(contractType == ENUM_CONTRACT_TYPE.ESTAG) {
+        legend.textContent = DESCRIPTIONS.PERSONAL_DATA;
+    }
+
+    if(contractType == ENUM_CONTRACT_TYPE.PJ) {
+        legend.textContent = DESCRIPTIONS.COMPANY_DATA;
+    }
+    
+    const div = document.createElement(TAGS.DIV);
+    div.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_STYLES.PURE_G);
+
+    const divChild = document.createElement(TAGS.DIV);
+    divChild.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_STYLES.PURE_U_1_MD_1_3);
+
+    const label = document.createElement(TAGS.LABEL);
+    label.setAttribute(ENUM_ATTRIBUTES.FOR,ENUM_ATTRIBUTES.MULTI_FIRST_NAME);
+    label.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_STYLES.CONTRACT_NAME);
+
+    if(contractType == ENUM_CONTRACT_TYPE.CLT) {
+        label.textContent = ENUM_PLACEHOLDER.NOME;
+    }
+
+    if(contractType == ENUM_CONTRACT_TYPE.PJ) {
+        label.textContent = ENUM_PLACEHOLDER.PJ;
+    }
+
+    if(contractType == ENUM_CONTRACT_TYPE.ESTAG) {
+        label.textContent = ENUM_PLACEHOLDER.ESTAG;
+    }
+
+
+    const input = document.createElement(TAGS.INPUT);
+    input.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_STYLES.PURE_INPUT_1);
+    input.setAttribute(ENUM_ATTRIBUTES.ID, ENUM_ATTRIBUTES.ID);
+    input.setAttribute(ENUM_ATTRIBUTES.TYPE_TEXT, ENUM_ATTRIBUTES.TYPE_TEXT);
+    input.setAttribute(ENUM_ATTRIBUTES.PLACEHOLDER, ENUM_PLACEHOLDER.NOME)
+    
+}
+
+
 function showCnpjField() {
-    document.querySelector('#isCnpj').style.display = 'none';
+    document.querySelector(SELECTORS.IS_CNPJ)
+    .style.display = ENUM_STYLES.DISPLAY_NONE;
 }
 showCnpjField(); 
 
@@ -9,19 +167,19 @@ const CreateField = {
     
     createInput(event) {     
         event.preventDefault();
-        const labelElement = document.createElement("label");
-        labelElement.innerHTML = 'Patrimônio';
+        const labelElement = document.createElement(TAGS.LABEL);
+        labelElement.innerHTML = TAGS.PATRIMONY;
 
-        const div = document.getElementById("items");
+        const div = document.getElementById(TAGS.items);
     
-        const child_div = document.createElement("div");
-        child_div.setAttribute("class", "pure-u-1 pure-u-md-1-3 mg-t-b-10");
-        child_div.setAttribute("id", `new-div-${CreateField.newField}`);
+        const child_div = document.createElement(TAGS.DIV);
+        child_div.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_STYLES.PURE_U_1_MD_1_3_MG_T_B_10);
+        child_div.setAttribute(ENUM_ATTRIBUTES.ID, `new-div-${CreateField.newField}`);
     
-        const input = document.createElement("input");
-        input.setAttribute("type", 'text');
-        input.setAttribute("class", "pure-input-1");
-        input.setAttribute("value", "-INFORME: Nome/número do equipamento");
+        const input = document.createElement(TAGS.INPUT);
+        input.setAttribute(ENUM_ATTRIBUTES.TYPE, ENUM_ATTRIBUTES.TYPE_TEXT);
+        input.setAttribute(ENUM_ATTRIBUTES.CLASS, ENUM_ATTRIBUTES.PURE_INPUT_1);
+        input.setAttribute(ENUM_ATTRIBUTES.VALUE, DESCRIPTIONS.ITEMS);
         
         child_div.appendChild(labelElement);
         child_div.appendChild(input);
@@ -35,11 +193,11 @@ const CreateField = {
 
 
        if(!document.querySelector('#new-div-'+CreateField.newField)) {
-            swal("Atenção", "Não existe field para remover"); return false;
+            swal(DESCRIPTIONS.WARNING, DESCRIPTIONS.NO_FIELD_REMOVE); return false;
        }
 
        if(CreateField.newField <= -2) {
-        swal("Atenção", "Não existe field para remover"); return false;
+        swal(DESCRIPTIONS.WARNING, DESCRIPTIONS.NO_FIELD_REMOVE); return false;
        }
        document.querySelector(`#new-div-${CreateField.newField}`).remove();
 
@@ -49,18 +207,18 @@ const CreateField = {
 
 const ChangeContractElement = {
     changeTitleContractDefault() {    
-        document.querySelector('.contract-type-clt').textContent = "CLT"
-        document.querySelector('.contract-type-estag').textContent = "Estagiário"
-        document.querySelector('.contract-name').textContent = "Nome"
+        document.querySelector(SELECTORS.CONTRACT_TYPE_CLT).textContent = DESCRIPTIONS.CLT
+        document.querySelector(SELECTORS.CONTRACT_TYPE_ESTAG).textContent = DESCRIPTIONS.ESTAG
+        document.querySelector(SELECTORS.CONTRACT_NAME).textContent = DESCRIPTIONS.NOME
       
-        document.querySelector('#isCnpj').style.display = 'none';
+        document.querySelector(SELECTORS.IS_CNPJ).style.display = ENUM_STYLES.NONE;
     },
     
     changeTitleContractBusiness() {        
-        document.querySelector('.contract-type-pj').textContent = "Pessoa Jurídica"
-        document.querySelector('.contract-name').textContent = "Nome Empresa"
+        document.querySelector(CONTRACT_TYPE_PJ).textContent = DESCRIPTIONS.PJ
+        document.querySelector(SELECTORS.CONTRACT_NAME).textContent = DESCRIPTIONS.NOME_EMPRESA
 
-        document.querySelector('#isCnpj').style.display = 'block';
+        document.querySelector(SELECTORS.IS_CNPJ).style.display = ENUM_STYLES.DISPLAY_BLOCK;
 
     },
 }
@@ -68,8 +226,8 @@ const ChangeContractElement = {
 const Form = {
 
     validateEmpty(value, name) {
-        if(value.trim() == "" || value.trim() == null) {
-            throw new Error("O campo " + name + " não pode ser nulo" );
+        if(!value.trim()) {
+            throw new Error(DESCRIPTIONS.FIELD + name + DESCRIPTIONS.NOT_NULL);
         }
     },
     
@@ -91,7 +249,7 @@ const Form = {
 
     prepareDocument(contractType, requerimentType, documentArray, addressArray, items) {
         
-        if(requerimentType == "AQ") {
+        if(requerimentType == ENUM_REQUERIMENT_TYPE.AQ) {
             Form.generateAcquisitionDocument(documentArray, addressArray, items, requerimentType)
         } 
         
@@ -263,7 +421,7 @@ const Form = {
             Form.prepareDocument(contractType, requerimentType, documentArray, addressArray, items)
        
         } catch(error) {
-            swal("Atenção!", error.message)
+            swal(DESCRIPTIONS.WARNING, error.message)
             return false;
         }
         
